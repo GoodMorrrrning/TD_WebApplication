53265d7f01afd3bc12bc769c8736e7c0d87e5151
GoodMorrrrning <51075493+GoodMorrrrning@users.noreply.github.com>
2020-12-07 09:51:24 +0100

    Ajoutez des fichiers projet.

---
+++ TD_WebApplication/Pages/etudiants.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TD_WebApplication.Pages
+{
+    public class etudiantsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
