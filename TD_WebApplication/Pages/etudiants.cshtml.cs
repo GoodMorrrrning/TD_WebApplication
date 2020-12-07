ff157654602baffc12a22b78520d468d5ca0b2a6
GoodMorrrrning <51075493+GoodMorrrrning@users.noreply.github.com>
2020-12-07 10:05:35 +0100

    ha

--- TD_WebApplication/Pages/etudiants.cshtml.cs
+++ TD_WebApplication/Pages/etudiants.cshtml.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using EpsiDTO;
 
 namespace TD_WebApplication.Pages
 {
     public class etudiantsModel : PageModel
     {
         public string coucou { get; set; }
+        public List<EtudiantDTO> ListeEtudiants = new List<EtudiantDTO>();
         public void OnGet()
         {
             coucou = "coucou";
